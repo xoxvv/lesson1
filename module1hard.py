710b989d419e7e870b48fca4f1ad300812df711d
xoxvv <166736344+xoxvv@users.noreply.github.com>
2024-05-14 02:27:44 +0600

    Add files via upload
    Дополнительное практическое задание по модулю*

---
+++ module1hard.py
@@ -0,0 +1,20 @@
+ball_sr={}
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students={'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+students_sp=list(students)
+print(students_sp)
+students_sp.sort()
+print(students_sp)
+grades[0]=sum(grades[0])/len(grades[0])
+grades[1]=sum(grades[1])/len(grades[1])
+grades[2]=sum(grades[2])/len(grades[2])
+grades[3]=sum(grades[3])/len(grades[3])
+grades[4]=sum(grades[4])/len(grades[4])
+print(grades)
+#ball_sr= {students_sp:grades}
+ball_sr['Aaron']=grades[0]
+ball_sr['Khendrik']=grades[3]
+ball_sr['Bilbo']=grades[1]
+ball_sr['Johnny']=grades[2]
+ball_sr['Steve']=grades[4]
+print(ball_sr)
