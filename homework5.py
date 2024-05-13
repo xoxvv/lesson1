714d471e169c3a85c96d50735eda2c8ebda51847
xoxvv <166736344+xoxvv@users.noreply.github.com>
2024-05-13 21:04:32 +0600

    Add files via upload
    Списки и словари

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list=["false",2,3,4,"apple"]
+print('my_list:', my_list)
+print('First_list=', my_list[0],'Last_list=', my_list[-1] )
+my_list[2]='kiwi'
+print('my_list:', my_list)
+
+my_dict={}
+my_dict['apple']='яблоко'
+my_dict['kiwi']='киви'
+print(my_dict)
+print("my_dict 'kiwi' =", my_dict['kiwi'])
+my_dict['apple']='hot apple'
+print(my_dict)
+my_dict
